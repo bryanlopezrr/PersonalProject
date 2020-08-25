c5e71888a127b6c18f092152b603c72634fd4616
bryanlopezrr <bryanlopezrr@gmail.com>
2020-08-25 17:01:34 -0400

    working on navBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './NavBar.css'
+
+
+function NavBar(){
+    return(
+        <>
+            <div className="navBar">
+                <ul>
+                    <li>Home</li>
+                    <li>Portfolio</li>
+                    <li>Resume</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
