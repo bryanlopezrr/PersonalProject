8d07ea3c571983b79cc92f76414845eb8bc89e0d
bryanlopezrr <bryanlopezrr@gmail.com>
2020-09-07 16:46:31 -0400

    changes to add the email service with command line arguments for credentials

---
+++ emailService/config.js
@@ -0,0 +1,32 @@
+function hidePassword(){
+
+    /* 
+    process.argv is an array containing the command line arguments. 
+    The first element will be 'node', the second element will be the name of the JavaScript file. 
+    The next elements will be any additional command line arguments.
+    */
+
+    password = process.argv[5];
+
+    console.log("THIS IS YOUR PASSWORD : " + password)
+
+    return password;
+}
+
+function hideUser(){
+    username = process.argv[4];
+
+    console.log("THIS IS YOUR USER NAME : " + username)
+
+    return username; 
+}
+
+module.exports = {
+    USER: hideUser(),
+    PASS: hidePassword()
+}
+
+
+
+
+//create a mock email to get all emails to isolate personal email from requests coming from the website
